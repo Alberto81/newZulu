e7ccfffcdff8f7b82f8e167e8a472e5b24ffc94d
Alberto Antelo <albertoantelopresa@gmail.com>
2014-05-07 12:44:58 +0200

    a objeto le añado peso y si se puede coger o no

--- Objeto.java
+++ Objeto.java
@@ -10,21 +10,33 @@ public class Objeto
     // instance variables - replace the example below with your own
     private String objeto;
     private float pesoObjeto;
+    private boolean seCoge;
 
     /**
      * Constructor for objects of class Objeto
      */
-    public Objeto(String objeto, float pesoObjeto)
+    public Objeto(String objeto, float pesoObjeto, boolean seCoge)
     {
         this.objeto = objeto;
         this.pesoObjeto = pesoObjeto;
+        this.seCoge = seCoge;
     }
 
+    public boolean getSeCoge()
+    {
+    return seCoge;
+    }
+    
     public void describeObjeto()
     {
         if(objeto != null){
-            System.out.println("There are "+objeto+". Pesa "+pesoObjeto+" kg." );
+            System.out.println(objeto+". Pesa "+pesoObjeto+" kg." );
         }
     }
 
+    public float getPeso()
+    {
+    return pesoObjeto;
+    }
+    
 }
