ead343356e24b216b1cc5f48afd450fed988ee71
Alberto Antelo <albertoantelopresa@gmail.com>
2014-05-07 12:41:34 +0200

    añadida clase jugador que contiene un array de objetos llamado mochila

---
+++ Player.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Objeto> mochila;
+    public static final float PESO_MAX = 50.0F;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        mochila = new ArrayList<Objeto>();
+    }
+
+   
+
+
+}
