12cc6310d95ebc652d96e7ff8fc92bfb4eca53fa
Alberto Antelo <albertoantelopresa@gmail.com>
2014-05-19 09:13:47 +0200

    he quitado todo el codigo que he encontrado que solo tenia razón de ser si podia haber un null como comando

--- Parser.java
+++ Parser.java
@@ -56,21 +56,17 @@ public class Parser
 
         // Find up to two words on the line.
         Scanner tokenizer = new Scanner(inputLine);
-        if(tokenizer.hasNext()) {
+    
             word1 = commands.getCommandWord(tokenizer.next());      // get first word
             if(tokenizer.hasNext()) {
                 word2 = tokenizer.next();      // get second word
                 // note: we just ignore the rest of the input line.
             }
-        }
+       
 
         // Now check whether this word is known. If so, create a command
         // with it. If not, create a "null" command (for unknown command).
-        if(commands.isCommand(word1)){
+     
             return new Command(word1, word2);
-        }
-        else {
-            return new Command(Option.UNKNOWN, word2); 
-        }
     }
 }
