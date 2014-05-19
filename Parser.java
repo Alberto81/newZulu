dbcea7dd2bdd9b08a0d954e0d6275c88f6e3c9f5
Alberto Antelo <albertoantelopresa@gmail.com>
2014-05-19 09:58:17 +0200

    simplificando genere un error en parser de forma que el programa saltaba cuando presionaba ener sin escribir ningun valor, solucionado

--- Parser.java
+++ Parser.java
@@ -29,18 +29,19 @@ public class Parser
         commands = new CommandWords();
         reader = new Scanner(System.in);
     }
-    
+
     //solucion 1 de 01116
     public CommandWords getCommands()
     {
-     return commands;   
-     }
-     
+        return commands;   
+    }
+
     //solucion 2 de 01116
     public void imprimeComandos()
     {
-      commands.showAll();
+        commands.showAll();
     }
+
     /**
      * @return The next command from the user.
      */
@@ -56,17 +57,17 @@ public class Parser
 
         // Find up to two words on the line.
         Scanner tokenizer = new Scanner(inputLine);
-    
+        if(tokenizer.hasNext()) {
             word1 = commands.getCommandWord(tokenizer.next());      // get first word
             if(tokenizer.hasNext()) {
                 word2 = tokenizer.next();      // get second word
                 // note: we just ignore the rest of the input line.
             }
-       
 
+        }
         // Now check whether this word is known. If so, create a command
         // with it. If not, create a "null" command (for unknown command).
-     
-            return new Command(word1, word2);
+
+        return new Command(word1, word2);
     }
 }
